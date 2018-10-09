844c31d3ca7180eee1e510984514630c965d4889
Titas <titasurbonas@gmai.com>
2018-10-09 20:08:05 +0200

    Added ex3,4,5

---
+++ Assignment2/Exercise3,4,5/Lab_4.ip_user_files/sim_scripts/system/ies/run.f
@@ -0,0 +1,123 @@
+-makelib ies_lib/xilinx_vip -sv \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/axi_vip_if.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/clk_vip_if.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/xilinx_vip/hdl/rst_vip_if.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "/opt/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
+  "/opt/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "/opt/Xilinx/Vivado/2018.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/axi_infrastructure_v1_1_0 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/5bb9/hdl/sc_util_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/axi_protocol_checker_v2_0_3 -sv \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/03a9/hdl/axi_protocol_checker_v2_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/axi_vip_v1_1_3 -sv \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/b9a8/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/processing_system7_vip_v1_0_5 -sv \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/70fd/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_processing_system7_0_0/sim/system_processing_system7_0_0.v" \
+-endlib
+-makelib ies_lib/axi_lite_ipif_v3_0_4 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/cced/hdl/axi_lite_ipif_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/lib_cdc_v1_0_2 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/ef1e/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib ies_lib/interrupt_control_v3_1_4 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/8e66/hdl/interrupt_control_v3_1_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/axi_gpio_v2_0_19 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/c193/hdl/axi_gpio_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_axi_gpio_0_0/sim/system_axi_gpio_0_0.vhd" \
+-endlib
+-makelib ies_lib/proc_sys_reset_v5_0_12 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/f86a/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_rst_ps7_0_100M_1/sim/system_rst_ps7_0_100M_1.vhd" \
+  "../../../bd/system/ip/system_axi_gpio_0_1/sim/system_axi_gpio_0_1.vhd" \
+-endlib
+-makelib ies_lib/generic_baseblocks_v2_1_0 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/b752/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_register_slice_v2_1_17 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/6020/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_2 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/7aff/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_2 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/7aff/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_2 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/7aff/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib ies_lib/axi_data_fifo_v2_1_16 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/247d/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_crossbar_v2_1_18 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/15a3/hdl/axi_crossbar_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_xbar_0/sim/system_xbar_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/sim/system.vhd" \
+-endlib
+-makelib ies_lib/blk_mem_gen_v8_3_6 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/2751/simulation/blk_mem_gen_v8_3.v" \
+-endlib
+-makelib ies_lib/axi_bram_ctrl_v4_0_14 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/6db1/hdl/axi_bram_ctrl_v4_0_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_axi_bram_ctrl_0_0/sim/system_axi_bram_ctrl_0_0.vhd" \
+-endlib
+-makelib ies_lib/blk_mem_gen_v8_4_1 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/67d8/simulation/blk_mem_gen_v8_4.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_axi_bram_ctrl_0_bram_0/sim/system_axi_bram_ctrl_0_bram_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ipshared/eea9/hdl/led_ip_v1_0_S_AXI.vhd" \
+  "../../../bd/system/ipshared/eea9/hdl/led_ip_v1_0.vhd" \
+  "../../../bd/system/ip/system_led_ip_0_0/sim/system_led_ip_0_0.vhd" \
+  "../../../bd/system/ipshared/62ff/hdl/matrix_ip_v1_0.vhd" \
+  "../../../bd/system/ipshared/62ff/hdl/matrix_ip_v1_0_S_AXI.vhd" \
+  "../../../bd/system/ip/system_matrix_ip_0_1/sim/system_matrix_ip_0_1.vhd" \
+-endlib
+-makelib ies_lib/axi_protocol_converter_v2_1_17 \
+  "../../../../Lab_4.srcs/sources_1/bd/system/ipshared/ccfb/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/system/ip/system_auto_pc_3/sim/system_auto_pc_3.v" \
+  "../../../bd/system/ip/system_auto_pc_2/sim/system_auto_pc_2.v" \
+  "../../../bd/system/ip/system_auto_pc_1/sim/system_auto_pc_1.v" \
+  "../../../bd/system/ip/system_auto_pc_0/sim/system_auto_pc_0.v" \
+  "../../../bd/system/ip/system_auto_pc_4/sim/system_auto_pc_4.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ Assignment2/Exercise3,4,5/Lab_4.sdk/lab4/Debug/src/Matrix.d
@@ -0,0 +1,48 @@
+src/Matrix.o src/Matrix.o: ../src/Matrix.c ../src/../inc/Matrix.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_mem.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/bspconfig.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/matrix_ip.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_io.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../standalone_bsp_3/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../src/../inc/Matrix.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_mem.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/bspconfig.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/matrix_ip.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_io.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../standalone_bsp_3/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
